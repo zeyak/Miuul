109abbbb26ac22b1df4c6efe016b02379c8e8813
zeynepkurtw <zeynepkurt.w@gmail.com>
2024-01-22 20:25:48 +0100

    update

--- scripts/3_DataAnalysis/OG_analysis.py
+++ scripts/3_DataAnalysis/OG_analysis.py
@@ -23,14 +23,14 @@ df_count_s = pd.concat([df_count, df_sing], axis=0)
 df_count_s.loc[df_count_s["Total"] > 1, "Type"] = "OG"
 df_count_s.loc[df_count_s["Total"] == 1, "Type"] = "singleton"
 
-df_count_s = df_count_s.rename(columns={"S_salmonicida_aa": "S. salmonicida",
-                                        "G_intestinalis_aa": "G. intestinalis",
-                                        "G_muris_aa": "G. muris"})
+df_count_s = df_count_s.rename(columns={"S_salmonicida_aa": "S.salmonicida",
+                                        "G_intestinalis_aa": "G.intestinalis",
+                                        "G_muris_aa": "G.muris"})
 
 "Upset plot for both OG and singletons marked different colors"
-df_stack = df_count_s.set_index(df_count_s["S. salmonicida"] >= 1). \
-    set_index(df_count_s["G. intestinalis"] >= 1, append=True). \
-    set_index(df_count_s["G. muris"] >= 1, append=True)
+df_stack = df_count_s.set_index(df_count_s["G.intestinalis"] >= 1). \
+    set_index(df_count_s["G.muris"] >= 1, append=True). \
+    set_index(df_count_s["S.salmonicida"] >= 1, append=True)
 
 """ plot upset0 """
 upset0 = UpSet(df_stack.sort_values(by="Total", ascending=True),
@@ -46,7 +46,7 @@ upset0.add_stacked_bars(by="Type",
 
 upset0.style_subsets(max_degree=1,
                      facecolor="white",
-                     edgecolor="black",
+                     edgecolor="pink",
                      label="Species-specific")
 
 sns.set_style("whitegrid", {'axes.grid': False})
